0f3bd699dc2ff24750bcd30c21622e18ca4b2ce9
ArthasDragon <1641627793@qq.com>
2019-08-30 10:48:16 +0800

    update

--- dragonshop/src/main/java/com/dragonshop/controller/UserController.java
+++ dragonshop/src/main/java/com/dragonshop/controller/UserController.java
@@ -11,12 +11,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Random;
+
 @Controller("user")
 @RequestMapping("/user")
+@CrossOrigin
 public class UserController extends BaseController{
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private HttpServletRequest httpServletRequest;
+
+    //用户获取otp短信接口
+    @RequestMapping(value = "/getopt", method = {RequestMethod.POST}, consumes = {CONTENT_TYPE_FORMED})
+    @ResponseBody
+    public CommonReturnType getOpt(@RequestParam(name = "telPhone")String telPhone){
+        //需要按照一定的规则生成opt验证码
+        Random random = new Random();
+        int randomInt = random.nextInt(99999);
+        randomInt += 10000;
+        String optCode = String.valueOf(randomInt);
+
+        //将otp验证码同对应用户的手机号关联   redis 或者  session
+        httpServletRequest.getSession().setAttribute(telPhone,optCode);
+
+
+        //将otp验证码通过短信通道发送给用户，省略
+
+        System.out.println(telPhone + ':' + optCode);
+        return CommonReturnType.create(null);
+    }
+
 
     @RequestMapping("/get")
     @ResponseBody
