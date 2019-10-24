2e391c3ee95b815b160c037f80a07401607ff511
ArthasDragon <1641627793@qq.com>
2019-10-24 16:26:51 +0800

    learn spring

---
+++ jikeSpring/multiSources/src/main/java/dragon/spring/multiSources/MultiSourcesApplication.java
@@ -0,0 +1,13 @@
+package dragon.spring.multiSources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiSourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiSourcesApplication.class, args);
+	}
+
+}
---
+++ jikeSpring/multiSources/src/test/java/dragon/spring/multiSources/MultiSourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package dragon.spring.multiSources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiSourcesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
