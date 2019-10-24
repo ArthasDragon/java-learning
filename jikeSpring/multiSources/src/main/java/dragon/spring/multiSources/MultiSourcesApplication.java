e33417bfba309aeba9ae98f5aa9429499cf344d6
ArthasDragon <1641627793@qq.com>
2019-10-24 16:42:52 +0800

    learn spring

--- jikeSpring/multiSources/src/main/java/dragon/spring/multiSources/MultiSourcesApplication.java
+++ jikeSpring/multiSources/src/main/java/dragon/spring/multiSources/MultiSourcesApplication.java
@@ -1,13 +1,34 @@
 package dragon.spring.multiSources;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 @SpringBootApplication
-public class MultiSourcesApplication {
+@Slf4j
+public class MultiSourcesApplication implements CommandLineRunner {
+	@Autowired
+	private DataSource dataSource;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MultiSourcesApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception{
+		showConnection();
+	}
+
+	private void showConnection() throws SQLException{
+		log.info(dataSource.toString());
+		Connection conn = dataSource.getConnection();
+		log.info(conn.toString());
+		conn.close();
+	}
 }
