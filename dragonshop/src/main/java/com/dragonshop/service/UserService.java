adcf061c48cc53a3d263e806e331e0730b92290a
ArthasDragon <1641627793@qq.com>
2019-11-29 16:24:49 +0800

    miaosha

--- dragonshop/src/main/java/com/dragonshop/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.dragonshop.service;
-
-import com.dragonshop.service.model.UserModel;
-
-public interface UserService {
-
-    //通过用户id获取用户对象的方法
-    UserModel getUserById(Integer id);
-}
--- multiSources/src/main/java/com/dragon/demo/multiDataSource/first/UserOneRepository.java
+++
@@ -1,6 +0,0 @@
-package com.dragon.demo.multiDataSource.first;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserOneRepository extends CrudRepository<UserOne, Integer> {
-}
