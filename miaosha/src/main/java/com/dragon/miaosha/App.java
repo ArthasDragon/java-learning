dcf969bef609fdde4f4fdd341c89ec6e6bcdaf62
ArthasDragon <1641627793@qq.com>
2019-12-02 14:45:07 +0800

    miaosha

--- miaosha/src/main/java/com/dragon/miaosha/App.java
+++ miaosha/src/main/java/com/dragon/miaosha/App.java
@@ -1,7 +1,11 @@
 package com.dragon.miaosha;
 
+import com.dragon.miaosha.dao.UserDOMapper;
+import com.dragon.miaosha.dataobject.UserDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,14 +14,23 @@ import org.springframework.web.bind.annotation.RestController;
  *
  */
 
-@EnableAutoConfiguration
+@SpringBootApplication(scanBasePackages = {"com.dragon.miaosha"})
 @RestController
+@MapperScan("com.dragon.miaosha.dao")
 public class App 
 {
 
+    @Autowired
+    private UserDOMapper userDOMapper;
+
     @RequestMapping("/")
     public String home(){
-        return "hello world";
+        UserDO userDO = userDOMapper.selectByPrimaryKey(1);
+        if(userDO == null){
+            return "用户对象不存在";
+        }else {
+            return userDO.getName();
+        }
     }
     public static void main( String[] args )
     {
